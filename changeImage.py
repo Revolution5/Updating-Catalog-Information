68cc702357794dfb6e900cd8fcc7c5fd1cfd3f9d
Damon <damonboone99@gmail.com>
2022-03-17 13:59:29 -0700

    Split the image filename and do proper adjustments

--- changeImage.py
+++ changeImage.py
@@ -3,7 +3,15 @@
 from PIL import Image
 import os
 
-imageDirectory = "/supplier-data/images/"
+imageDirectory = "supplier-data/images/"
 
 for image in os.listdir(imageDirectory):
-        im = Image.open(imageDirectory + image)
+        #example directory contains images with .tiff extension
+        if (image.endswith(".tiff")):
+            im = Image.open(imageDirectory + image)
+            imSplit = image.split(".")
+            imNew = imSplit[0] + ".jpeg"
+            #resize image to 600x400, convert image to RGB from RGBA, save as a .JPEG
+            im.resize((600,400))
+            im.convert("RGB")
+            im.save(imageDirectory + imNew, "JPEG")
