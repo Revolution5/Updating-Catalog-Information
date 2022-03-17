d55ce966f95666db4079807c8dd4753075a525d9
Damon <damonboone99@gmail.com>
2022-03-17 15:34:37 -0700

    Add comment

--- supplier_image_upload.py
+++ supplier_image_upload.py
@@ -5,6 +5,7 @@ import os
 imageDirectory = "supplier-data/images/"
 url = "http://localhost/upload/" #ip address is given by the lab
 
+#loops thru directory and uploads jpegs to test server
 for image in os.listdir(imageDirectory):
     if (image.endswith("jpeg")):
         with open(imageDirectory + image, "rb") as opened:
